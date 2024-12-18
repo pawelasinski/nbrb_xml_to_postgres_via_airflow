db8acebf9d8f2bfb7d8c7a1e9976121bc13be8df
Pavel Asinskiy <p.asinsky@syberry.com>
2024-12-18 16:56:40 +0300

    refined init_db.sh

--- docker_postgres/init_db.sh
+++ docker_postgres/init_db.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 
-set -e
+set -e  # Exit immediately if a command exits with a non-zero status.
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
 CREATE TABLE IF NOT EXISTS nbrb_rates_daily_basis
   (
-    id text NOT NULL,
-    date text NOT NULL,
-    numcode text NOT NULL,
-    charcode text NOT NULL,
-    scale text NOT NULL,
-    name text NOT NULL,
-    rate text NOT NULL
+    id TEXT NOT NULL,
+    date TEXT NOT NULL,
+    numcode TEXT NOT NULL,
+    charcode TEXT NOT NULL,
+    scale TEXT NOT NULL,
+    name TEXT NOT NULL,
+    rate TEXT NOT NULL
   );
 EOSQL
+# -v ON_ERROR_STOP=1 — it is a flag that tells psql to stop execution if an error occurs.
