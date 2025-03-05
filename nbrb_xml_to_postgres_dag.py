46f2e4a8ec9b1ebd103d14872d19278a2e28b93a
pawelasinski <pawel.asinski@gmail.com>
2025-03-05 11:56:47 +0300

    Refined nbrb_xml_to_postgres_dag.py

--- nbrb_xml_to_postgres_dag.py
+++ nbrb_xml_to_postgres_dag.py
@@ -1,134 +1,183 @@
 """This DAG is for moving official exchange rates of the Belarusian ruble against foreign currencies
 set by the National Bank of the Republic of Belarus on a daily basis from XML to Postgres.
 
-Todo:
-    * Make possible to extract the rates only for business days, i.e. excepting weekends and holiday national days. You need to twick schedule_interval, or implement BranchOperator or ShortCircuitOperator.
-
 """
+
 import os
 import csv
 import logging
 from datetime import datetime
 from xml.etree import ElementTree
 
+import holidays
+from psycopg2 import DataError, OperationalError
+
 from airflow import DAG
 from airflow.utils.dates import days_ago
 from airflow.utils.edgemodifier import Label
 from airflow.operators.bash import BashOperator
-from airflow.operators.python_operator import PythonOperator, ShortCircuitOperator
+from airflow.operators.python import PythonOperator, ShortCircuitOperator
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-path_xml_dump = '/tmp/nbrb_rates.xml'
-path_csv_conv = '/tmp/nbrb_rates.csv'
-url_nbrb_rates = 'https://services.nbrb.by/xmlexrates.aspx?ondate={{ ds }}'
+PATH_XML_DUMP = "/tmp/nbrb_rates.xml"
+PATH_CSV_CONV = "/tmp/nbrb_rates.csv"
+URL_NBRB_RATES_TEMPLATE = "https://services.nbrb.by/xmlexrates.aspx?ondate={{ ds }}"
 
 DEFAULT_ARGS = {
-    'start_date': days_ago(30),
-    'owner': 'pawelasinski',
-    'doc_md': __doc__,
+    "start_date": days_ago(30),
+    "owner": "pawelasinski",
+    "doc_md": __doc__,
 }
 
 with DAG(
-        dag_id='nbrb_xml_to_postgres_dag',
+        dag_id="nbrb_xml_to_postgres_dag",
         default_args=DEFAULT_ARGS,
-        tags=['tutorial'],
-        schedule_interval='0 0 * * 1-6',
+        tags=["asinski"],
+        schedule_interval="@daily",
         max_active_runs=1,
-):
-    def is_weekday_func(execution_dt):
-        return datetime.strptime(execution_dt, '%Y-%m-%d').weekday() in range(0, 5)
+        catchup=True,
+) as dag:
+    def is_business_day_func(execution_dt: str) -> bool:
+        """Check if the given date is a business day (not a weekend and not a Belarusian national holiday)."""
+        date_ = datetime.strptime(execution_dt, "%Y-%m-%d").date()
+        belarus_holidays = holidays.BY(years=date_.year)
+        if date_.weekday() >= 5:  # 5 = Saturday, 6 = Sunday
+            logger.info("%s is a weekend day.", execution_dt)
+            return False
+        if date_ in belarus_holidays:
+            logger.info("%s is a holiday: %s", execution_dt, belarus_holidays.get(date_))
+            return False
+        logger.info("%s is a business day.", execution_dt)
+        return True
 
 
-    is_weekday = ShortCircuitOperator(
-        task_id='is_weekday',
-        python_callable=is_weekday_func,
-        op_kwargs={'execution_dt': '{{ ds }}'}
+    is_business_day = ShortCircuitOperator(
+        task_id="is_business_day",
+        python_callable=is_business_day_func,
+        op_kwargs={"execution_dt": "{{ ds }}"},
     )
 
 
-    def check_if_data_exists_func(execution_date):
-        pg_hook = PostgresHook(postgres_conn_id='asinski_postgres_conn')
-
+    def does_data_exist_func(execution_date: str) -> bool:
+        """Check if data for the given date already exists in the `nbrb_rates_daily_basis` table."""
+        pg_hook = PostgresHook(postgres_conn_id="postgres_conn")
         with pg_hook.get_conn() as conn:
-            cursor = conn.cursor()
-
-            query = 'SELECT EXISTS (SELECT 1 FROM nbrb_rates_daily_basis WHERE date = %s)'
-            cursor.execute(query, (execution_date,))
-            exists = cursor.fetchone()[0]
-
-            cursor.close()
-
+            with conn.cursor() as curs:
+                query = "SELECT EXISTS (SELECT 1 FROM nbrb_rates_daily_basis WHERE date = %s)"
+                curs.execute(query, (execution_date,))
+                exists = curs.fetchone()[0]
+        conn.close()
+        if exists:
+            logger.info("Data for %s already exists in the database.",
+                        execution_date)
+        else:
+            logger.info("No data for %s found. Proceeding with the load.",
+                        execution_date)
         return not exists
 
 
-    check_if_data_exists = ShortCircuitOperator(
-        task_id='check_if_data_exists',
-        python_callable=check_if_data_exists_func,
-        op_kwargs={'execution_date': '{{ ds }}'},
+    does_data_exist = ShortCircuitOperator(
+        task_id="does_data_exist",
+        python_callable=does_data_exist_func,
+        op_kwargs={"execution_date": "{{ ds }}"},
     )
 
     download_xml_nbrb_rates = BashOperator(
-        task_id='download_xml_nbrb_rates',
-        bash_command=f'curl "{url_nbrb_rates}" > {path_xml_dump}'
+        task_id="download_xml_nbrb_rates",
+        bash_command=f'curl "{URL_NBRB_RATES_TEMPLATE}" > {PATH_XML_DUMP}',
     )
 
 
-    def convert_xml_to_csv_func():
-        parser = ElementTree.XMLParser(encoding='utf-8')
-        tree = ElementTree.parse(f'{path_xml_dump}', parser=parser)
-        root = tree.getroot()
+    def convert_xml_to_csv_func() -> None:
+        """Convert XML data to CSV format."""
+        try:
+            parser = ElementTree.XMLParser(encoding="utf-8")
+            tree = ElementTree.parse(PATH_XML_DUMP, parser=parser)
+            root = tree.getroot()
+        except ElementTree.ParseError as e:
+            logger.error("Error parsing XML: %s", e)
+            raise
 
-        with open(f'{path_csv_conv}', 'w') as csv_file:
-            csv_writer = csv.writer(csv_file)
-            for currency in root.findall('Currency'):
-                numcode = currency.find('NumCode').text
-                charcode = currency.find('CharCode').text
-                scale = currency.find('Scale').text
-                name = currency.find('Name').text
-                rate = currency.find('Rate').text
-
-                the_whole_curr_row = [
-                    currency.attrib['Id'], root.attrib['Date'],
-                    numcode, charcode, scale, name, rate
-                ]
-                csv_writer.writerow(the_whole_curr_row)
-
-                logging.info(the_whole_curr_row)
+        with open(PATH_CSV_CONV, "w", encoding="utf-8") as f_csv:
+            csv_writer = csv.writer(f_csv)
+            for currency in root.findall("Currency"):
+                try:
+                    curr_id = currency.attrib.get("Id", "NA")
+                    date_value = root.attrib.get("Date", "")
+                    numcode = (currency.find("NumCode").text
+                               if currency.find("NumCode") is not None else "")
+                    charcode = (currency.find("CharCode").text
+                                if currency.find("CharCode") is not None else "")
+                    scale = (currency.find("Scale").text
+                             if currency.find("Scale") is not None else "")
+                    name = (currency.find("Name").text
+                            if currency.find("Name") is not None else "")
+                    rate = (currency.find("Rate").text
+                            if currency.find("Rate") is not None else "")
+                    row = [curr_id, date_value, numcode, charcode, scale, name, rate]
+                    csv_writer.writerow(row)
+                    logger.info(f"The row has been converted from XML to CSV: %s")
+                except Exception as e:
+                    logger.error("Error processing currency entry: %s", e)
+                    continue
 
 
     convert_xml_to_csv = PythonOperator(
-        task_id='convert_xml_to_csv',
+        task_id="convert_xml_to_csv",
         python_callable=convert_xml_to_csv_func,
     )
 
 
-    def load_csv_into_postgres_func():
-        pg_hook = PostgresHook(postgres_conn_id='asinski_postgres_conn')
-
-        with open(f'{path_csv_conv}', 'r') as csv_file:
-            pg_hook.copy_expert(sql="COPY nbrb_rates_daily_basis FROM stdin DELIMITER ','",
-                                filename=csv_file.name)
+    def load_csv_into_postgres_func() -> None:
+        """Load the CSV file into the `nbrb_rates_daily_basis` table in the Postgres database."""
+        try:
+            pg_hook = PostgresHook(postgres_conn_id="postgres_conn")
+            with open(PATH_CSV_CONV, encoding="utf-8") as f_csv:
+                sql_copy_query = 'COPY nbrb_rates_daily_basis FROM STDIN WITH (FORMAT csv, DELIMITER ",")'
+                pg_hook.copy_expert(sql=sql_copy_query, filename=f_csv.name)
+            logger.info("Data has been loaded successfully into the `exchange_rate_db` database.")
+        except FileNotFoundError as e:
+            logger.error("Error loading CSV file: %s", e)
+            raise
+        except OperationalError as e:
+            logger.error("Error to connect to the `nbrb_rates_daily_basis` database: %s",
+                         e)
+            raise
+        except DataError as e:
+            logger.error("Data error occurred (possible schema mismatch): %s", e)
+            raise
+        except Exception as e:
+            logger.exception("Error loading CSV into Postgres: %s", e)
+            raise
 
 
     load_csv_into_postgres = PythonOperator(
-        task_id='load_csv_into_postgres',
+        task_id="load_csv_into_postgres",
         python_callable=load_csv_into_postgres_func,
     )
 
 
-    def delete_temp_files_func():
-        os.remove(path_xml_dump)
-        os.remove(path_csv_conv)
+    def delete_temp_files_func() -> None:
+        """Delete temporary files created during the process."""
+        for file_path in [PATH_XML_DUMP, PATH_CSV_CONV]:
+            try:
+                if os.path.exists(file_path):
+                    os.unlink(file_path)
+                    logger.info("%s has been deleted.", file_path)
+                else:
+                    logger.warning("%s has not been found for deletion.", file_path)
+            except Exception as e:
+                logger.exception("Error deleting %s: %s", file_path, e)
 
 
     delete_temp_files = PythonOperator(
-        task_id='delete_temp_files',
+        task_id="delete_temp_files",
         python_callable=delete_temp_files_func,
-        trigger_rule='all_done'
+        trigger_rule="all_done",
     )
 
-    is_weekday >> check_if_data_exists >> download_xml_nbrb_rates >> Label("XML to CSV") >> convert_xml_to_csv >> Label(
-        "CSV to Postgres") >> load_csv_into_postgres >> delete_temp_files
+    is_business_day >> does_data_exist >> download_xml_nbrb_rates >> Label(
+        "XML to CSV") >> convert_xml_to_csv >> Label("CSV to Postgres") >> load_csv_into_postgres >> delete_temp_files
